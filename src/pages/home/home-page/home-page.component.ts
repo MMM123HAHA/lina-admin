04a1d9f995dd84f0f318c777324251fa598416fa
lina <1079867114@QQ.COM>
2017-07-26 22:43:08 +0800

    save arezzo files

--- src/pages/home/home-page/home-page.component.ts
+++ src/pages/home/home-page/home-page.component.ts
@@ -8,44 +8,44 @@ import { Meta, Title } from '@angular/platform-browser';
 })
 export class HomePageComponent implements OnInit {
   users:Array<any> = [
-    {"index":19,
-    "name":"刘雨飏",
+    {"index":1,
+    "name":"张三",
       "sex":"Male",
-      "name_en":"Ryane",
-      "github":"ryanemax",
-      "exam1":66,
-      "exam2":66,
-      "exam3":66},
-      {"index":7,
-      "name":"小明",
-      "sex":"Male",
-      "name_en":"Ryane",
-      "github":"ryanemax",
-      "exam1":66,
-      "exam2":66,
-      "exam3":66},
-      {"index":14,
-      "name":"Chunk",
-      "sex":"Male",
-      "name_en":"Chunk",
-      "github":"chunk",
-      "exam1":76,
-      "exam2":76,
-      "exam3":76}
+      "name_en":"Tom",
+      "skill":"Java",
+      "working_year":"10",
+      "education":"本科",
+      "homepage":"zhangsan"},
+     {"index":2,
+     "name":"李雷",
+      "sex":"Female",
+      "name_en":"Lily",
+      "skill":"C++",
+      "working_year":"12",
+      "education":"本科",
+      "homepage":"lilei"},
+      {"index":3,
+     "name":"李花",
+      "sex":"Female",
+      "name_en":"Monica",
+      "skill":"C",
+      "working_year":"8",
+      "education":"本科",
+      "homepage":"lihua"}
   ]
 deleteLast(){
   this.users.pop()
 }
 saveNewUser(){
   this.users.push({
-     "index":666,
-      "name":"New User",
-      "sex":"Male",
-      "name_en":"new_user",
-      "github":"new_user",
-      "exam1":76,
-      "exam2":76,
-      "exam3":76
+      "index":11,
+      "name":"李花",
+      "sex":"Female",
+      "name_en":"Monica",
+      "skill":"C",
+      "working_year":"8",
+      "education":"本科",
+      "homepage":"lihua"
   })
 }
   sortByAsccending(){
@@ -62,28 +62,33 @@ saveNewUser(){
   });
   }
 
-
-
   sortByDesccending(){
     // 参考MDN Array操作的API文档 Array相关方法
     // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array
- this.users.sort(function (a, b) {
-  if (a.index > b.index) {
-    return -1;
+    this.users.sort(function (a, b) {
+      if (a.index > b.index) {
+        return -1;
+      }
+      if (a.index < b.index) {
+        return 1;
+      }
+      // a 必须等于 b
+      return 0;
+      });
   }
-  if (a.index < b.index) {
-    return 1;
-  }
-  // a 必须等于 b
-  return 0;
-  });
-}
 
   sortByRadom(){
     // 参考MDN Array操作的API文档 Math相关方法
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+    function shuffle(a) {
+      for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+      }
+    }
+    shuffle(this.users);
+    }
 
-  }
   constructor(meta: Meta, title: Title) {
     title.setTitle('My Home Page');
 
